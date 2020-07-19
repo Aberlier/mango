36cf3d160c987d9ea31b05ce754ef77f70dfd5da
ChiLi-zy <1162995411@qq.com>
2020-07-19 14:50:53 +0800

    我提上去  保存一份  增加跨域

---
+++ src/main/java/com/mangocms/mango/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.mangocms.mango.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @program: mango
+ * @description: 实现跨域
+ * @author: zjc
+ * @create: 2020-07-19 11:15
+ **/
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") //允许跨域访问的路径
+        .allowedOrigins("*") //允许跨域访问的源
+        .allowedMethods("POST","GET","PUT","POTIONS","DELETE") //允许请求方法
+        .maxAge(168000)//预检间隔时间
+        .allowedHeaders("*") //允许头部设置
+        .allowCredentials(true); //是否发送cookie
+    }
+}
